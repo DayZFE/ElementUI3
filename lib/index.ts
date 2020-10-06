a196b39eaa6b76b4e6805ed6118ae2fb53f959f2
zcating <zcating@qq.com>
2020-10-06 22:51:34 +0800

    tmp commit

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import { App } from 'vue';
-import Cdk from './cdk';
-
-export * from './ele-ui-provider';
-=======
 import { App } from "vue";
-
-export * from "./ele-ui-provider";
 import Cdk from "./cdk";
 
+export * from "./ele-ui-provider";
 export { default as Alert } from "./alert";
->>>>>>> 4c925f1d6908a00d39b9d66c7c8bcb859cab8c46
 
 export default {
   install(app: App, ...options: any[]) {
